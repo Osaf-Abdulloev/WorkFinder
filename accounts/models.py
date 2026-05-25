7f4d576a64908d40d76334bd61e62b4dfd77b510
Osaf <abdulloevosaf0@gmail.com>
2026-05-25 17:59:02 +0500

    Add registration and send code to gmail

--- accounts/models.py
+++ accounts/models.py
@@ -1,21 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 # Create your models here.
 
 
 
+class EmailConfirm(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6)
+    
+    def __str__(self):
+        return self.user.username
+    
+
+
 class User(AbstractUser):
     role_choice =[
-        ('Seeker', 'seeker'),
-        ('Employer', 'employer')
+        ('seeker', 'Seeker'),
+        ('employer', 'Employer')
     ]
     
-    role = models.CharField(choices=role_choice)
-    age = models.PositiveIntegerField()
-    phone = models.CharField(max_length=13)
+    role = models.CharField(max_length=25,choices=role_choice, null=True, blank=True )
+    age = models.PositiveIntegerField(null=True, blank=True)
+    phone = models.CharField(max_length=13,null=True, blank=True)
     adress = models.CharField(max_length=30, null=True, blank=True)
-    img = models.ImageField(upload_to='users/')
-    create_at = models.DateTimeField(auto_now=True)
+    img = models.ImageField(upload_to='users/', default='def.jpg',null=True, blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
     
     
     def __str__(self):
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,154 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from django.contrib.auth import authenticate, login, logout
+import random
+from .models import EmailConfirm
+from django.core.mail import send_mail
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
 
 # Create your views here.
+
+
+
+def sendcode(user):
+    
+    code = random.randint(100000, 999999)
+    
+    EmailConfirm.objects.create(user_id = user.id, code = code) 
+    
+    try:
+        subject = 'Confirm Your Password | JobFinder'
+
+        from_email = settings.DEFAULT_FROM_EMAIL
+        
+        to = [user.email]
+        
+        
+        text_content = f'''
+        Hello {user.username}
+        
+        Your confirmation code is: {code}
+        '''
+        
+        
+        html_content = f'''
+        <div style="
+            font-family: Arial;
+            max-width: 600px;
+            margin: auto;
+            padding: 30px;
+            background: #f4f4f4;
+        ">
+        
+            <div style="
+                background: white;
+                padding: 30px;
+                border-radius: 12px;
+            ">
+        
+                <h1 style="color:#2563eb;">
+                    JobFinder
+                </h1>
+        
+                <h2>
+                    Password Confirmation
+                </h2>
+        
+                <p>
+                    Hello <b>{user.username}</b>,
+                </p>
+        
+                <p>
+                    Use the confirmation code below:
+                </p>
+        
+                <div style="
+                    font-size: 32px;
+                    font-weight: bold;
+                    letter-spacing: 5px;
+                    background:#eff6ff;
+                    padding:20px;
+                    border-radius:10px;
+                    text-align:center;
+                    color:#2563eb;
+                ">
+                    {code}
+                </div>
+        
+                <p style="margin-top:25px;">
+                    If you did not request this email,
+                    you can safely ignore it.
+                </p>
+        
+                <hr>
+        
+                <p style="color:gray;">
+                    © JobFinder Team
+                </p>
+        
+            </div>
+        </div>
+        '''
+        
+        
+        msg = EmailMultiAlternatives(
+            subject,
+            text_content,
+            from_email,
+            to
+        )
+        
+        msg.attach_alternative(html_content, "text/html")
+        
+        msg.send()
+    except Exception as e:
+         print(e, '========================================')
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        pas1 = request.POST.get('pas1')
+        pas2 = request.POST.get('pas2')
+        email = request.POST.get('email')
+        role = request.POST.get('role')
+        age = request.POST.get('age')
+        phone = request.POST.get('phone')
+        adress = request.POST.get('adress')
+        if request.FILES.get('img'):
+            img = request.FILES.get('img')
+        else:
+            img = None
+        
+        if User.objects.filter(username = username).exists():
+            return render(request, 'acc/reg.html', context={'eror' : 'Username already exists'})
+        
+        if pas1!=pas2:
+            return render(request, 'acc/reg.html', context={'eror' : 'Password`s not equal'})
+        
+        if User.objects.filter(email = email).exists():
+            return render(request, 'acc/reg.html', context={'eror' : f'User with email {email} already exists'})
+        
+        if int(age) < 18:
+            return render(request, 'acc/reg.html', context={'eror' : f'You are a child comeback after {18 - int(age)} years'})
+        
+        user = User.objects.create_user(
+            username=username,
+            password=pas1,
+            role=role,
+            email=email,
+            age = age,
+            phone = phone,
+            adress = adress,
+            img = img)
+        
+        user.save()
+        
+        sendcode(user)
+        return redirect('/')
+    
+    return render(request, 'acc/reg.html')
+        
+
+
+
